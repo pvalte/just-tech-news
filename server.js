9a1a1a214870d7d7fbbfe4b80c847f7fd4b1b3ec
Paige A <pvaltemare@gmail.com>
2021-11-12 19:15:42 -0800

    set up files and user routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+//"sync" means Sequelize is taking the models and connecting them to associated database tables. 
+//If it doesn't find a table, it'll create it for you!
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
