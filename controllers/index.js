efa999fd393b70b4c5e666c24e15292d5ae78d70
Paige A <pvaltemare@gmail.com>
2021-11-15 18:13:36 -0800

    add homepage handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 //collecting the packaged group of API endpoints and prefixing them with the path /api
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 //if we make a request to any endpoint that doesn't exist, we'll receive a 404 error 
 router.use((req, res) => {
