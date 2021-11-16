cd34275b8eedcb194b89cc9f5ed4c92370ede83e
Paige A <pvaltemare@gmail.com>
2021-11-15 18:49:25 -0800

    add sessions

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -12,6 +24,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
